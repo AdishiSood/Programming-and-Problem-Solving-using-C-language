2d52de02ab02d73fdb8f00124f9e8bd689357586
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-06 22:19:02 +0530

    Create Matrix_Pattern.c

---
+++ Pyramids and Patterns/Matrix_Pattern.c
@@ -0,0 +1,38 @@
+/*
+
+1  2  4  7
+3  5  8 11
+6  9 12 14
+10 13 15 16
+
+*/
+
+#include <stdio.h>
+int main()
+{
+  int n, p = 1, a[100][100], j, m, k, r;
+
+  scanf("%d", &r);
+
+  for (j = 1; j <= r; j++) {
+    m = 0;
+    n = j;
+    for (k = 1; k <= j; k++)
+      a[m++][--n] = p++;
+  }
+
+  for (j = 1; j <= r-1; j++) {
+    m = j;
+    n = r-1;
+    for (k = 1; k<= r-j; k++)
+      a[m++][n--] = p++;
+  }
+
+  for (j = 0; j <= r-1; j++) {
+    for (k = 0; k <= r-1; k++)
+      printf("%d ", a[j][k]);
+    printf("\n");
+  }
+
+  return 0;
+}
