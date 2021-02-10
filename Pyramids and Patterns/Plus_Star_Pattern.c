e26983e893cd12b65422e8aa41bd3720f6ffb4e9
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-11 01:26:34 +0530

    Create Plus_Star_Pattern.c

---
+++ Pyramids and Patterns/Plus_Star_Pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>  
+  
+int main() 
+{  
+  int n;  
+  printf("Enter the odd number only");  
+  scanf("%d", &n);  
+  for(int i=1;i<=n;i++)  
+  {  
+    if(i==((n/2)+1))  
+    {  
+      for(int j=1;j<=n;j++)  
+      {  
+        printf("+");  
+      }  
+   
+    }  
+    else  
+    {  
+    for(int j=1;j<=n/2;j++)  
+    {  
+      printf(" ");  
+    }  
+    printf("+");  
+    }  
+    printf("\n");  
+  }  
+  return 0;  
+}  
