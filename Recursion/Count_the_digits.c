7e0a52017a8a993886038ce9bbd348758b395532
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-19 11:24:29 +0530

    Update Count_the_digits.c

--- Recursion/Count_the_digits.c
+++ Recursion/Count_the_digits.c
@@ -2,13 +2,11 @@
 Write a program in C to count the digits of a given number using recursion.
 */
 #include<stdio.h>
-
 int noOfDigits(int n1);
 int main()
 {
   int n1,ctr;
     printf("\n\n count the digits of a given number :\n");
-	printf("-----------------------------------------\n");
     printf(" Input  a number : ");
     scanf("%d",&n1);
 
